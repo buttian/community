6a92945e416e804f3af7d449d262312ad5ad8f6f
butian <1120185190@qq.com>
2022-08-03 17:57:46 +0800

    首页

---
+++ src/main/java/com/sss/community/dao/AlphaDao.java
@@ -0,0 +1,5 @@
+package com.sss.community.dao;
+
+public interface AlphaDao {
+    String select();
+}
---
+++ src/main/java/com/sss/community/dao/AlphaDaoMyBatisImpl.java
@@ -0,0 +1,13 @@
+package com.sss.community.dao;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Primary
+public class AlphaDaoMyBatisImpl implements AlphaDao {
+    @Override
+    public String select() {
+        return "MyBatis";
+    }
+}
